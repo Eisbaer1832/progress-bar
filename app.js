e9908131e0ca1b9d7d103a671643557a42e42139
eisbaer1832 <eisbaer1832@gmail.com>
2022-03-29 18:17:06 +0200

    mehr bugs

--- app.js
+++ app.js
@@ -22,21 +22,366 @@ app.listen(port, () => {
 });
 
 app.post('/public/data', function(req, res) {
-  let maxHöhe = req.body.maxHöhe;
-  console.log("maxHöhe: "+ maxHöhe );
+  let maxHoehe = req.body.maxHoehe;
+  console.log("maxHoehe: "+ maxHoehe );
   
   try {
-    fs.writeFileSync(Object.keys({maxHöhe})+".txt", maxHöhe);
+    fs.writeFileSync(Object.keys({maxHoehe})+".txt", maxHoehe);
   } catch (err) {
     console.error(err);
   };
 
 });
 
-app.post('/public/smaxhoehe', function(req, res) {
+
+app.post('/public/smaxhoehe1', function(req, res) {
     console.log("klappt")
-    let maxHöhe = fs.readFileSync(("maxHöhe")+ ".txt")
-    console.log("gelesene maxhöhe : " +maxHöhe)
-    res.send(maxHöhe);
+    let maxHoehe = fs.readFileSync(("maxHoehe")+ ".txt")
+    res.send(maxHoehe);
 
 });
+
+
+app.post('/public/shoehe', function(req, res) {
+  console.log("klappt")
+  let hoehe = fs.readFileSync(("hoehe")+ ".txt")
+  res.send(hoehe);
+
+});
+
+
+app.post('/public/smaxHoehe2', function(req, res) {
+  console.log("klappt")
+  let maxHoehe2 = fs.readFileSync(("maxHoehe2")+ ".txt")
+  res.send(maxHoehe2);
+
+});
+
+app.post('/public/sname1', function(req, res) {
+  console.log("klappt")
+  let name1 = fs.readFileSync(("name1")+ ".txt")
+  res.send(name1);
+
+});
+
+
+app.post('/public/sname2', function(req, res) {
+  console.log("klappt")
+  let name2 = fs.readFileSync(("name2")+ ".txt")
+  res.send(name2);
+
+});
+
+app.post('/public/sname3', function(req, res) {
+  console.log("klappt")
+  let name3 = fs.readFileSync(("name3")+ ".txt")
+  res.send(name3);
+
+});
+
+app.post('/public/sname4', function(req, res) {
+  console.log("klappt")
+  let name4 = fs.readFileSync(("name4")+ ".txt")
+  res.send(name4);
+
+});
+
+app.post('/public/sname5', function(req, res) {
+  console.log("klappt")
+  let name5 = fs.readFileSync(("name5")+ ".txt")
+  res.send(name5);
+
+});
+
+app.post('/public/sname6', function(req, res) {
+  console.log("klappt")
+  let name6 = fs.readFileSync(("name6")+ ".txt")
+  res.send(name6);
+
+});
+
+app.post('/public/sheight1', function(req, res) {
+  console.log("klappt")
+  let height1 = fs.readFileSync(("height1")+ ".txt")
+  res.send(height1);
+
+});
+
+app.post('/public/sheight2', function(req, res) {
+  console.log("klappt")
+  let height2 = fs.readFileSync(("height2")+ ".txt")
+  res.send(height2);
+
+});
+
+app.post('/public/sheight3', function(req, res) {
+  console.log("klappt")
+  let height3 = fs.readFileSync(("height3")+ ".txt")
+  res.send(height3);
+
+});
+
+app.post('/public/sheight4', function(req, res) {
+  console.log("klappt")
+  let height4 = fs.readFileSync(("height4")+ ".txt")
+  res.send(height4);
+
+});
+
+
+app.post('/public/sheight5', function(req, res) {
+  console.log("klappt")
+  let height5 = fs.readFileSync(("height5")+ ".txt")
+  res.send(height5);
+
+});
+
+app.post('/public/sheight6', function(req, res) {
+  console.log("klappt")
+  let height6 = fs.readFileSync(("height6")+ ".txt")
+  res.send(height6);
+
+});
+
+app.post('/public/serledigt', function(req, res) {
+  console.log("klappt")
+  let erledigt = fs.readFileSync(("erledigt")+ ".txt")
+  res.send(erledigt);
+
+});
+
+app.post('/public/scurrentgoal', function(req, res) {
+  console.log("klappt")
+  let currentgoal = fs.readFileSync(("currentgoal")+ ".txt")
+  res.send(currentgoal);
+
+});
+
+app.post('/public/snochzutuen', function(req, res) {
+  console.log("klappt")
+  let nochzutuen = fs.readFileSync(("nochzutuen")+ ".txt")
+  res.send(nochzutuen);
+
+});
+
+
+
+
+
+
+app.post('/public/maxHoehe2', function(req, res) {
+  let maxHoehe2 = req.body.maxHoehe2;
+  console.log("maxHoehe2: "+ maxHoehe2 );
+  
+  try {
+    fs.writeFileSync(Object.keys({maxHoehe2})+".txt", maxHoehe2);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+app.post('/public/hoehe', function(req, res) {
+  let hoehe = req.body.hoehe;
+  console.log("hoehe: "+ hoehe );
+  
+  try {
+    fs.writeFileSync(Object.keys({hoehe})+".txt", hoehe);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+
+app.post('/public/name1', function(req, res) {
+  let name1 = req.body.name1;
+  console.log("name1: "+ name1 );
+  
+  try {
+    fs.writeFileSync(Object.keys({name1})+".txt", name1);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+
+app.post('/public/name2', function(req, res) {
+  let name2 = req.body.name2;
+  console.log("name2: "+ name2 );
+  
+  try {
+    fs.writeFileSync(Object.keys({name2})+".txt", name2);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+
+app.post('/public/name3', function(req, res) {
+  let name3 = req.body.name3;
+  console.log("name3: "+ name3 );
+  
+  try {
+    fs.writeFileSync(Object.keys({name3})+".txt", name3);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+app.post('/public/name4', function(req, res) {
+  let name4 = req.body.name4;
+  console.log("name4: "+ name4 );
+  
+  try {
+    fs.writeFileSync(Object.keys({name4})+".txt", name4);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+
+app.post('/public/name5', function(req, res) {
+  let name5 = req.body.name5;
+  console.log("name5: "+ name5 );
+  
+  try {
+    fs.writeFileSync(Object.keys({name5})+".txt", name5);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+app.post('/public/name6', function(req, res) {
+  let name6 = req.body.name6;
+  console.log("name6: "+ name6 );
+  
+  try {
+    fs.writeFileSync(Object.keys({name6})+".txt", name6);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+app.post('/public/height1', function(req, res) {
+  let height1 = req.body.height1;
+  console.log("height1: "+ height1 );
+  
+  try {
+    fs.writeFileSync(Object.keys({height1})+".txt", height1);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+app.post('/public/height2', function(req, res) {
+  let height2 = req.body.height2;
+  console.log("height2: "+ height2 );
+  
+  try {
+    fs.writeFileSync(Object.keys({height2})+".txt", height2);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+
+
+app.post('/public/height3', function(req, res) {
+  let height3 = req.body.height3;
+  console.log("height3: "+ height3 );
+  
+  try {
+    fs.writeFileSync(Object.keys({height3})+".txt", height3);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+
+
+app.post('/public/height4', function(req, res) {
+  let height4 = req.body.height4;
+  console.log("height4: "+ height4 );
+  
+  try {
+    fs.writeFileSync(Object.keys({height4})+".txt", height4);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+
+app.post('/public/height5', function(req, res) {
+  let height5 = req.body.height5;
+  console.log("height5: "+ height5 );
+  
+  try {
+    fs.writeFileSync(Object.keys({height5})+".txt", height5);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+
+app.post('/public/height6', function(req, res) {
+  let height6 = req.body.height6;
+  console.log("height6: "+ height6 );
+  
+  try {
+    fs.writeFileSync(Object.keys({height6})+".txt", height6);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+app.post('/public/erledigt', function(req, res) {
+  let erledigt = req.body.erledigt;
+  console.log("erledigt: "+ erledigt );
+  
+  try {
+    fs.writeFileSync(Object.keys({erledigt})+".txt", erledigt);
+    console.log(erledigt)
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+
+app.post('/public/currentgoal', function(req, res) {
+  let currentgoal = req.body.currentgoal;
+  console.log("currentgoal: "+ currentgoal );
+  
+  try {
+    fs.writeFileSync(Object.keys({currentgoal})+".txt", currentgoal);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
+app.post('/public/nochzutuen', function(req, res) {
+  let nochzutuen = req.body.nochzutuen;
+  console.log("nochzutuen: "+ nochzutuen );
+  
+  try {
+    fs.writeFileSync(Object.keys({nochzutuen})+".txt", nochzutuen);
+  } catch (err) {
+    console.error(err);
+  };
+
+});
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
-let höhe
-let maxHöhe
-let maxHöhe2
-let name
-let name2
-let name3
+let hoehe 
+let maxHoehe 
+let maxHoehe2 
+let name1 
+let name2 
+let name3 
 let name4
-let name5 
+let name5
 let name6
-let height
+let height1
 let height2
 let height3
 let height4
@@ -17,9 +17,7 @@ let erledigt
 let currentgoal
 let nochzutuen
 
-
-
-
+load()
 
 var toastTrigger = document.getElementById('liveToastBtn')
 var toastLiveExample = document.getElementById('liveToast')      
@@ -27,18 +25,18 @@ const termometer = document.getElementById("Termometer");
 var toastLiveExample2 = document.getElementById('liveToast2')      
 const gift = document.querySelector('#gift');
 
-console.log("Maximale Höhe " + maxHöhe)
+console.log("Maximale Hoehe " + maxHoehe)
 console.log("Schon Erledigt " + erledigt) 
-console.log("Höhe " + höhe)
+console.log("Hoehe " + hoehe)
 console.log("current gaol: " + currentgoal)
 
-termometer.setAttribute("style","background:url(/public/assets/red.png); background-repeat: repeat-x;  background-position: 1000px "+höhe+"px");
+termometer.setAttribute("style","background:url(/public/assets/red.png); background-repeat: repeat-x;  background-position: 1000px "+hoehe+"px");
 document.getElementById("erledigt").innerHTML = erledigt;
-document.getElementById("nochzutuen").innerHTML = maxHöhe-erledigt;
+document.getElementById("nochzutuen").innerHTML = maxHoehe-erledigt;
 goal();
 document.getElementById("goal-text").innerHTML = currentgoal;
 
-min = (Math.min(height, height2, height3, height4, height5, height6));
+min = (Math.min(height1, height2, height3, height4, height5, height6));
 
 
 
@@ -50,6 +48,18 @@ if (erledigt < min) {
         gift.addEventListener('animationend', () => {
             gift.classList.remove("animate__wobble", "animate__animated", "animate__repeat-3");
             gift.classList.add("animate__animated", "animate__repeat-1", "animate__bounceOutDown");
+            $.ajax({
+              dataType: "JSON",
+              traditional: true,
+              type: 'POST',
+              data:{'currentgift': currentgift},
+              cache: false,
+              url: '/public/currentgift',
+              success: function (currentgift) {
+                console.log(currentgift);
+                currentgift = currentgift
+              }
+            });
             document.getElementById("currentgift").innerHTML = currentgift;
             currentgift.classList.add("animate__animated", "animate__repeat-1","animate__bounceOutDown" )
       });
@@ -58,38 +68,44 @@ if (erledigt < min) {
 
 
 
-function MaximaleHöhe()
+function MaximaleHoehe()
 { 
-  let maxHöhe = Number(document.getElementById("maxHöhe").value); ;
+  let maxHoehe = Number(document.getElementById("maxHoehe").value); ;
 
-  document.getElementById("nochzutuen").innerHTML = maxHöhe-erledigt;
+  document.getElementById("nochzutuen").innerHTML = maxHoehe-erledigt;
 
   $.ajax({
     dataType: "JSON",
     traditional: true,
     type: 'POST',
-    data:{'maxHöhe': maxHöhe},
+    data:{'maxHoehe': maxHoehe},
     cache: false,
-    url: '/public/data',
+    url: '/public/maxHoehe',
+    data:{'maxHoehe': maxHoehe},
   });
-
-  $.ajax({
-    dataType: "JSON",
-    traditional: true,
-    type: 'POST',
-    url: '/public/smaxhoehe',
-    success: function (maxHöhe) {
-      console.log(maxHöhe);
-    }
-  });
+  save()
+  load()
 }
 
-function MaximaleHöhe2()
+function MaximaleHoehe2()
 { 
-  maxHöhe = document.getElementById("maxHöhe2").value;
-  console.log(maxHöhe)
+  maxHoehe = document.getElementById("maxHoehe2").value;
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/maxHoehe2',
+    cache: false,
+    data:{'maxHoehe': maxHoehe2},
+    success: function (currentgift) {
+      console.log(maxHoehe2);
+      maxHoehe2 = maxHoehe2
+    }
+  });
+  save()
+  console.log(maxHoehe)
   document.getElementById("erledigt").innerHTML = erledigt;
-  document.getElementById("nochzutuen").innerHTML = maxHöhe-erledigt
+  document.getElementById("nochzutuen").innerHTML = maxHoehe-erledigt
 
 
 }
@@ -97,44 +113,47 @@ function MaximaleHöhe2()
 function reset()
 {
   erledigt = 0
-  höhe = 640
-  maxHöhe = 0
+  hoehe = 640
+  maxHoehe = 0
 
   console.log(erledigt); 
-  console.log(höhe);
-  termometer.setAttribute("style","background:url(/public/assets/red.png); background-repeat: repeat-x;  background-position: 1000px "+höhe+"px");
+  console.log(hoehe);
+  termometer.setAttribute("style","background:url(/public/assets/red.png); background-repeat: repeat-x;  background-position: 1000px "+hoehe+"px");
   document.getElementById("erledigt").innerHTML = erledigt;
-  document.getElementById("nochzutuen").innerHTML = maxHöhe-erledigt;
+  document.getElementById("nochzutuen").innerHTML = maxHoehe-erledigt;
   setgoal()
   goal()
+  save()
 }
 
-function höher() 
+function hoeher() 
 { 
+  load()
   var toast = new bootstrap.Toast(toastLiveExample)
-  höhe = höhe - (630 / maxHöhe)
+  hoehe = hoehe - (630 / maxHoehe)
   
 
   erledigt++
   console.log("Erledigt: " + erledigt); 
-  console.log("Höhe: " + höhe);
-  console.log("MaxHöhe: " + maxHöhe);
-  termometer.setAttribute("style","background:url(/public/assets/red.png); background-repeat: repeat-x; background-position: 1000px "+höhe+"px;");
+  console.log("Hoehe: " + hoehe);
+  console.log("MaxHoehe: " + maxHoehe);
+  termometer.setAttribute("style","background:url(/public/assets/red.png); background-repeat: repeat-x; background-position: 1000px "+hoehe+"px;");
 
   document.getElementById("erledigt").innerHTML = erledigt;
-  document.getElementById("nochzutuen").innerHTML = maxHöhe-erledigt;
+  document.getElementById("nochzutuen").innerHTML = maxHoehe-erledigt;
 
   toast.show()
   goal()
+  save()
 }
 
 function setgoal()
 {
-    
+    load()
     var name = document.getElementById("goal-name-1").value 
-    var height = document.getElementById("goal-height-1").value 
+    var height1 = document.getElementById("goal-height-1").value 
     const line1 = document.getElementById("line1");
-    line1.setAttribute("style","top:"+ -((height*maxHöhe)/640)+"px;");
+    line1.setAttribute("style","top:"+ -((height*maxHoehe)/640)+"px;");
 
 
     
@@ -158,20 +177,21 @@ function setgoal()
     var height6 = document.getElementById("goal-height-6").value 
 
 
-    console.log(height, height2, height3, height4, height5, height6)
+    console.log(height1, height2, height3, height4, height5, height6)
     document.getElementById("goal-text").innerhtml = "Nächster Preis:" + name;
     var currentgoal = name
     document.getElementById("goal-text").innerHTML = "Nächster Preis: " + currentgoal;
+    save()
 
 }
 function goal()
 {
-
+    load()
     console.log("goal function executed")
     var toast2 = new bootstrap.Toast(toastLiveExample2)
     
  
-    min = (Math.min(height, height2, height3, height4, height5, height6));
+    min = (Math.min(height1, height2, height3, height4, height5, height6));
     console.log("min: " + min)
     console.log("Schon Erledigt " + erledigt) 
 
@@ -192,16 +212,36 @@ function goal()
     }
 
 
-    console.log("height " + height)
+    console.log("height1 " + height1)
 
         
-    if (height == erledigt){
+    if (height1 == erledigt){
         console.log("erledigt: " +erledigt)
-        height = 99999
-        console.log("height: " + height)
+        height1 = 99999
+        console.log("height1: " + height1)
         document.getElementById("goal-text").innerHTML = "Nächster Preis: " + name2;
+        $.ajax({
+          dataType: "JSON",
+          traditional: true,
+          type: 'POST',
+          url: '/public/height1',
+          data:{'height1': height1},
+          success: function (height1) {
+            console.log(height1);
+            height1 = height1
+          }
+        });
         var currentgoal = name2
-
+        $.ajax({
+          dataType: "JSON",
+          traditional: true,
+          type: 'POST',
+          url: '/public/name2',
+          success: function (name2) {
+            console.log(name2);
+            name2 = name2
+          }
+        });
         
         
     }
@@ -252,7 +292,454 @@ function goal()
 
     }
     
+    $.ajax({
+      dataType: "JSON",
+      traditional: true,
+      type: 'POST',
+      data:{'height1': height1},
+      url: '/public/height1',
+      success: function (height1) {
+        console.log(height1);
+        height1 = height1
+      }
+    });
 
-    console.log(height, height2, height3, height4, height5, height6)
-    console.log(name, name2, name3, name4, name5, name6)
+    $.ajax({
+      dataType: "JSON",
+      traditional: true,
+      type: 'POST',
+      data:{'height2': height2},
+      url: '/public/height2',
+      success: function (height2) {
+        console.log(height2);
+        height2= height2
+      }
+    });
+
+
+    $.ajax({
+      dataType: "JSON",
+      traditional: true,
+      type: 'POST',
+      data:{'height3': height3},
+      url: '/public/height3',
+      success: function (height3) {
+        console.log(height3);
+        height3= height3
+      }
+    });
+
+
+    $.ajax({
+      dataType: "JSON",
+      traditional: true,
+      type: 'POST',
+      data:{'height4': height4},
+      url: '/public/height4',
+      success: function (height4) {
+        console.log(height4);
+        height4= height4
+      }
+    });
+    
+    
+    $.ajax({
+      dataType: "JSON",
+      traditional: true,
+      type: 'POST',
+      data:{'height5': height5},
+      url: '/public/height5',
+      success: function (height5) {
+        console.log(height5);
+        height5= height5
+      }
+    });
+    
+    
+    $.ajax({
+      dataType: "JSON",
+      traditional: true,
+      type: 'POST',
+      data:{'height6': height6},
+      url: '/public/height6',
+      success: function (height6) {
+        console.log(height6);
+        height6= height6
+      }
+    });
+    console.log(height1, height2, height3, height4, height5, height6)
+    console.log(name1, name2, name3, name4, name5, name6)
 }
+
+
+function load(){
+  console.log("load")
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'maxHoehe': maxHoehe},
+    url: '/public/smaxhoehe1',
+    success: function (maxHoehe) {
+      console.log(maxHoehe);
+      maxHoehe = maxHoehe
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/smaxHoehe2',
+    success: function (maxHoehe2) {
+      console.log(maxHoehe2);
+      maxHoehe2 = maxHoehe2
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/shoehe',
+    success: function (hoehe) {
+      console.log(hoehe);
+      hoehe = hoehe
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sname1',
+    success: function (name1) {
+      console.log(name1);
+      name1 = name1
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sname2',
+    success: function (name2) {
+      console.log(name2);
+      name2 = name2
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sname3',
+    success: function (name3) {
+      console.log(name3);
+      name3 = name3
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sname4',
+    success: function (name4) {
+      console.log(name4);
+      name4 = name4
+    }
+  });
+  
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sname5',
+    success: function (name5) {
+      console.log(name5);
+      name5 = name5
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sname6',
+    success: function (name6) {
+      console.log(name6);
+      name6 = name6
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sheight1',
+    success: function (height1) {
+      console.log(height1);
+      height1 = height1
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sheight2',
+    success: function (height2) {
+      console.log(height2);
+      height2 = height2
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sheight3',
+    success: function (height3) {
+      console.log(height3);
+      height3 = height3
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sheight4',
+    success: function (height4) {
+      console.log(height4);
+      height4 = height4
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sheight5',
+    success: function (height5) {
+      console.log(height5);
+      height5 = height5
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/sheight6',
+    success: function (height6) {
+      console.log(height6);
+      height6 = height6
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/erledigt',
+    success: function (erledigt) {
+      console.log(erledigt);
+      erledigt = erledigt
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/currentgoal',
+    success: function (currentgoal) {
+      console.log(currentgoal);
+      currentgoal = currentgoal
+    }
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    url: '/public/nochzutuen',
+    success: function (nochzutuen) {
+      console.log(nochzutuen);
+      nochzutuen = nochzutuen
+    }
+  });
+
+}
+
+function save(){
+
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'maxHoehe': maxHoehe},
+    cache: false,
+    url: '/public/data',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'maxHoehe2': maxHoehe2},
+    cache: false,
+    url: '/public/maxHoehe2',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'hoehe': hoehe},
+    cache: false,
+    url: '/public/hoehe',
+  });
+
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'name1': name1},
+    cache: false,
+    url: '/public/name1',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'name2': name2},
+    cache: false,
+    url: '/public/name2',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'name3': name3},
+    cache: false,
+    url: '/public/name3',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'name4': name4},
+    cache: false,
+    url: '/public/name4',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'name5': name5},
+    cache: false,
+    url: '/public/name5',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'name6': name6},
+    cache: false,
+    url: '/public/name6',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'height1': height1},
+    cache: false,
+    url: '/public/height1',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'height2': height2},
+    cache: false,
+    url: '/public/height2',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'height3': height3},
+    cache: false,
+    url: '/public/height3',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'height4': height4},
+    cache: false,
+    url: '/public/height4',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'height5': height5},
+    cache: false,
+    url: '/public/height5',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'height6': height6},
+    cache: false,
+    url: '/public/height6',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'erledigt': erledigt},
+    cache: false,
+    url: '/public/erledigt',
+  });
+
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'currentgoal': currentgoal},
+    cache: false,
+    url: '/public/currentgoal',
+  });
+
+  $.ajax({
+    dataType: "JSON",
+    traditional: true,
+    type: 'POST',
+    data:{'nochzutuen': nochzutuen},
+    cache: false,
+    url: '/public/nochzutuen',
+  });}
+
+
