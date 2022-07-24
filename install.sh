043bb3e4bfd0c67326667ea05cd4a1187d9b8466
Eisbaer1832 <65364427+Eisbaer1832@users.noreply.github.com>
2022-07-24 15:45:25 +0200

    Install Script improved

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#sudo npm install express io jquery jsdom mysql router socket.io supervisor favicon-serve
+sudo npm install express io jquery jsdom mysql router socket.io supervisor favicon-serve
 touch currentgift.txt
 touch goalpassed.txt
 touch currentgoal.txt
