decd398799ff7ccb12d9874ee87be92c96c76357
BuildTools <unconfigured@null.spigotmc.org>
2023-04-12 18:49:24 +0200

    fixes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express();
-const port = 3000;
+const port = 9876;
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const { json } = require('express');
