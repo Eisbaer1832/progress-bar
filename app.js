f64c7efeb1edab58c00275e2b18578c23cc3dc19
eisbaer1832 <eisbaer1832@gmail.com>
2024-06-09 21:53:42 +0200

    Transfered txt files to one JSON database. Loading and saving will now accure through ONE Ajax request and is so much more flexible and fast.

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.listen(port, () => {
 });
 
 
+
+
 /////////////////////////////
 //                         //
 //                         //
@@ -42,78 +44,15 @@ app.listen(port, () => {
 //                         //                                      
 //                         //                                        
 /////////////////////////////
+app.post('/public/data', function(req, res) {
+  let data = JSON.parse(fs.readFileSync("db.json", 'utf8'))
+  console.log(data)
+  res.send(data);
 
-
-app.post('/public/smaxhoehe', function(req, res) {
-    var lmaxHoehe = fs.readFileSync(("maxHoehe")+ ".txt")
-    res.send(lmaxHoehe);
-    
 });
-
-
-app.post('/public/sgoalpassed', function(req, res) {
-  var lgoalpassed = fs.readFileSync(("goalpassed")+ ".txt")
-  res.send(lgoalpassed);
-  
-});
-
-app.post('/public/saved_passwort', function(req, res) {
-  var lsaved_passwort = fs.readFileSync(("saved_passwort")+ ".txt")
-  res.send(lsaved_passwort);
-  
-});
-
-app.post('/public/shoehe', function(req, res) {
-  var lhoehe = fs.readFileSync(("hoehe")+ ".txt")
-  res.send(lhoehe);
-});
-
-
-app.post('/public/sname1', function(req, res) {
-  var lname1 = fs.readFileSync(("name1")+ ".txt")
-  res.send(lname1);
-  
-});
-
-
-app.post('/public/sheight1', function(req, res) {
-  
-  var lheight1 = fs.readFileSync(("height1")+ ".txt")
-  res.send(lheight1);
-});
-
-
-app.post('/public/serledigt', function(req, res) {
-  var lerledigt = fs.readFileSync(("erledigt")+ ".txt")
-  res.send(lerledigt);
-  
-});
-
-app.post('/public/scurrentgoal', function(req, res) {
-  var lcurrentgoal = fs.readFileSync(("currentgoal")+ ".txt")
-  res.send(lcurrentgoal);
-  
-});
-
-app.post('/public/scurrentgift', function(req, res) {
-  var lcurrentgift = fs.readFileSync(("currentgift")+ ".txt")
-  res.send(lcurrentgift);
-  
-});
-
-app.post('/public/snochzutuen', function(req, res) {
-  var lnochzutuen = fs.readFileSync(("nochzutuen")+ ".txt")
-  res.send(lnochzutuen);
-  
-});
-
-app.post('/public/lsavetime', function(req, res) {
-  var lsavetime = fs.readFileSync(("savetime")+ ".txt")
-  res.send(lsavetime);
-});
-
 
 app.post('/public/loadlogs', function(req, res) {
+  console.log(fs.readFileSync("logs.txt"))
   var logs = fs.readFileSync("logs.txt")
   res.send(logs); 
 });
@@ -132,98 +71,6 @@ app.post('/public/logs', function(req, res) {
   fs.appendFileSync("logs.txt", logs + "\n");
 });
 
-app.post('/public/goalpassed', function(req, res) {
-  var goalpassed = req.body.goalpassed;
-    fs.writeFileSync("goalpassed.txt", goalpassed);
-});
-
-app.post('/public/erledigt', function(req) {
-  var erledigt = req.body.erledigt;
-  
-  try {
-    fs.writeFileSync(Object.keys({erledigt})+".txt", erledigt);
-  } catch (err) {
-    console.error(err);
-  };
-
-});
-
-app.post('/public/savetime', function(req) {
-  var savetime = req.body.savetime;
-  
-  try {
-    fs.writeFileSync(Object.keys({savetime})+".txt", savetime);
-  } catch (err) {
-    console.error(err);
-  };
-
-});
-
-app.post('/public/maxHoehe', function(req) {
-  var maxHoehe = req.body.maxHoehe;
-  
-  try {
-    fs.writeFileSync(Object.keys({maxHoehe})+".txt", maxHoehe);
-  } catch (err) {
-    console.error(err);
-  };
-
-});
-
-app.post('/public/name1', function(req,res) {
-  var name1 = req.body.name1;
-
-  
-  try {
-    fs.writeFileSync(Object.keys({name1})+".txt", name1);
-  } catch (err) {
-    console.error(err);
-  };
-  res.send(name1);
-
-});
-
-
-app.post('/public/height1', function(req) {
-  var height1 = req.body.height1;
-  
-  try {
-    fs.writeFileSync(Object.keys({height1})+".txt", height1);
-  } catch (err) {
-    console.error(err);
-  };
-
-});
-
-
-
-app.post('/public/currentgift', function(req) {
-  var currentgift = req.body.currentgift;
-  
-  try {
-    fs.writeFileSync(Object.keys({currentgift})+".txt", currentgift);
-  } catch (err) {
-    console.error(err);
-  };
-
-});
-
-app.post('/public/nochzutuen', function(req) {
-  var nochzutuen = req.body.nochzutuen;
-  
-  try {
-    fs.writeFile (Object.keys({nochzutuen})+".txt", nochzutuen);
-  } catch (err) {
-    console.error(err);
-  };
-});
-
-app.post('/public/hoehe', function(req, res) {
-  var hoehe = req.body.hoehe;
-  try {
-    fs.writeFileSync(Object.keys({hoehe})+".txt", hoehe);
-  } catch (err) {
-    console.error(err);
-  };
-  res.send(hoehe);
+app.post('/public/savedata', function(req, res) {
+  fs.writeFileSync("db.json", req.body.data);
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,20 @@
 //TODO: Implement time stop when goal is reached and save current time to backend to set a highscore
+let hoehe, finishtime, maxHoehe, name1, height1, done, currentgoal, currentgift, saved_password, time_max, password, authorized, goalpassed, savetime, time_to_display, logs
+let data
 
-let hoehe, maxHoehe, name1, height1, height2, height3, height4, height5, height6, erledigt, currentgoal, currentgift, saved_passwort, time_max, passwort, authorized, goalpassed, savetime, saved, time_to_display, logs
-const timer_text = document.getElementById("timeleft")
 
 load()
-getTimestampInSeconds()
-currentgiftfunction()
-masheightCheck()
+document.getElementById("currentgift").innerHTML = name1;
+hoehe = 522 - ((510 / maxHoehe)*done)
 
-document.getElementById("erledigt").innerHTML = erledigt;
-document.getElementById("nochzutuen").innerHTML = maxHoehe-erledigt;
+document.getElementById("erledigt").innerHTML = done;
+document.getElementById("nochzutuen").innerHTML = maxHoehe-done;
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')      
 const termometer = document.getElementById("Termometer");
 const toastLiveExample2 = document.getElementById('liveToast2')      
 const gift = document.querySelector('#gift');
+const timer_text = document.getElementById("timeleft")
 
 
 //n_higher event listener
@@ -30,17 +30,23 @@ onresize = (event) => {
   navedit()
 }
 
+getTimestampInSeconds()
+
 
 async function getTimestampInSeconds () {
   await load()
-  console.log(savetime)
-  if (savetime != 0)
+  console.log(finishtime)
+  if (done < height1)
   {
 	  console.log("wuuuusch")
     while (true)
     {
+      let time
       time_max = 1738364399
-      let time = Math.floor(Date.now() / 1000)
+      time = Math.floor(Date.now() / 1000)
+
+
+      
       let time_left = new Date ((time_max-time))
       var d = Math.floor(time_left / (3600*24));
       var h = Math.floor(time_left % (3600*24) / 3600);
@@ -74,11 +80,11 @@ async function getTimestampInSeconds () {
 
     }
   }else{
-
-    var d = Math.floor(savetime / (3600*24));
-    var h = Math.floor(savetime % (3600*24) / 3600);
-    var m = Math.floor(savetime % 3600 / 60);
-    var s = Math.floor(savetime % 60);
+    console.log("done")
+    var d = Math.floor(finishtime / (3600*24));
+    var h = Math.floor(finishtime % (3600*24) / 3600);
+    var m = Math.floor(finishtime % 3600 / 60);
+    var s = Math.floor(finishtime % 60);
     if (h < 10) {h = "0" + h}
     if (d < 10) {d = "0" + d}
     if (m < 10) {m = "0" + m}
@@ -105,42 +111,39 @@ $('#Termometer').animate({
 
 
         
-if (height1 == 999999){
-  document.getElementById("bis-nächstes-ziel").innerHTML = "keine Ziele mehr"
+if (height1 <= done){
+  document.getElementById("bis-nächstes-ziel").innerHTML = "Ihr habt euer Ziel erreicht!"
 }else{
-  document.getElementById("bis-nächstes-ziel").innerHTML = height1 - erledigt;
+  document.getElementById("bis-nächstes-ziel").innerHTML = height1 - done;
 }
 
 
 function MaximaleHoehe()
 { 
   maxHoehe = Number(document.getElementById("maxHoehe").value);
-  document.getElementById("nochzutuen").innerHTML = maxHoehe-erledigt;
+  document.getElementById("nochzutuen").innerHTML = maxHoehe-done;
   console.log("maxHoehe: " + maxHoehe)
-  save_maxHoehe()
-}
+  save()}
 
 function MaximaleHoehe2()
 { 
   maxHoehe = document.getElementById("maxHoehe2").value;
-  document.getElementById("erledigt").innerHTML = erledigt;
-  document.getElementById("nochzutuen").innerHTML = maxHoehe-erledigt
-  save_maxHoehe()
+  document.getElementById("erledigt").innerHTML = done;
+  document.getElementById("nochzutuen").innerHTML = maxHoehe-done
+  save()
 }
 
 function reset()
 {
-  erledigt = 0
+  done = 0
   hoehe = 522
   goalpassed = "True"
 
   termometer.setAttribute("style","background:url(/public/assets/red.png); background-repeat: repeat-x;  background-position: 1000px "+hoehe+"px");
-  document.getElementById("erledigt").innerHTML = erledigt;
-  document.getElementById("nochzutuen").innerHTML = maxHoehe-erledigt;
+  document.getElementById("erledigt").innerHTML = done;
+  document.getElementById("nochzutuen").innerHTML = maxHoehe-done;
 
-  save_erledigt()
-  save_hoehe()
-  save_goalpassed()
+  save()
 
 }
 
@@ -150,78 +153,26 @@ function hoeher(count)
   toast = new bootstrap.Toast(toastLiveExample)
   hoehe = hoehe - ((510 / maxHoehe)*count)
 
-  erledigt = erledigt+1*count
+  done = done+1*count
 
   termometer.setAttribute("style","background:url(/public/assets/red.png); background-repeat: repeat-x; background-position: 1000px "+hoehe+"px;");
 
-  document.getElementById("erledigt").innerHTML = erledigt;
-  document.getElementById("nochzutuen").innerHTML = maxHoehe-erledigt;
+  document.getElementById("erledigt").innerHTML = done;
+  document.getElementById("nochzutuen").innerHTML = maxHoehe-done;
   toast.show()
   goal()
   logs=Date.now()/1000 + ";" + count
   save_logs()
-  masheightCheck()
-  save_erledigt()
-  save_hoehe()
-}
-
-function masheightCheck(){
-  if (erledigt >=
-     height1){
-    height1 = 999999
-    save_height1()
-  }
-
-  if (erledigt > height2){
-    height2 = 999998
-    save_height2()
-  }
-
-  if (erledigt > height3){
-    height3 = 999997
-    save_height3()
-  }
-  
-  
-  if (erledigt > height4){
-    height4 = 999996
-    save_height4()
-  }
-
-
-  if (erledigt > height5){
-    height5 = 999995
-    save_height5()
-  }
-
-  if (erledigt > height6){
-    height6 = 999994
-    save_height6()
-  }
-
-}
-
-function currentgiftfunction(){
-
-  if(height1 != 999999 && height2 != 999998 && height3 != 999997 && height4 != 999996 && height5 != 99996 && height6 != 99995){
-    currentgift = "Party im Restaurant Athos"
-    save_currentgift()
-    document.getElementById("currentgift").innerHTML = currentgift;
-  }else{
-    currentgift = "Der Wintergarten von Athos wird gemietet."
-    save_currentgift()
-    document.getElementById("currentgift").innerHTML = currentgift;
-  }
 }
 
 
-function passwortfunction()
+function passwordfunction()
 {
   var offcanvas = document.getElementById('offcanvas')
-  var toast = document.getElementById('passwortwrongtoast')
-  passwort = document.getElementById("passwort").value;
+  var toast = document.getElementById('passwordwrongtoast')
+  password = document.getElementById("password").value;
 
-  if (passwort == saved_passwort)
+  if (password == saved_password)
   {
     authorized = true
     var showOffcanvas = new bootstrap.Offcanvas(offcanvas)
@@ -238,8 +189,7 @@ function setgoal1()
   height1 = document.getElementById("goal-height-1").value 
   goalpassed = "True"
   goal()
-  save_height1()
-  save_name1()
+  save()
   document.getElementById("goal-text").innerhtml = "Nächster Preis:" + name1;
   currentgoal = name1
   document.getElementById("goal-text").innerHTML = "Nächster Preis: " + currentgoal;
@@ -252,33 +202,18 @@ function goal()
     toast2.show()
 
 
-    document.getElementById("bis-nächstes-ziel").innerHTML = height1 - erledigt;
-    if (erledigt == height1) {
-        document.getElementById("goal-text").innerHTML = "Verdienter preis:" + currentgift;
-        const gift = document.querySelector('#gift');
-        gift.classList.add("animate__wobble", "animate__animated", "animate__repeat-3");
-        gift.addEventListener('animationend', () => {
-            gift.classList.remove("animate__wobble", "animate__animated", "animate__repeat-3");
-            gift.classList.add("animate__animated", "animate__repeat-1", "animate__bounceOutDown");
-            document.getElementById("currentgift").innerHTML = currentgift;
-            document.getElementById("currentgift").classList.add("animate__animated", "animate__repeat-1","animate__bounceInDown", "goal-text");
-          });
+    document.getElementById("bis-nächstes-ziel").innerHTML = height1 - done;
 
-    }
-    if (height1 <= erledigt){
-        document.getElementById("bis-nächstes-ziel").innerHTML = "keine Ziele mehr"
+    if (height1 <= done){
+        document.getElementById("bis-nächstes-ziel").innerHTML = "Ihr habt euer Ziel erreicht";
         savetime = time_max - Math.floor(Date.now() / 1000)
         console.log(savetime)
-        save_savetime()
-        height1 = 999999
         currentgift = name1
-        currentgoal = "es gibt keine Ziele mehr"
-        save_height1()
-        save_currentgoal()
-        save_currentgift()
-        
-    }
+        currentgoal = "Ihr habt euer Ziel erreicht"
+        finishtime = time_max - Math.floor(Date.now() / 1000)
 
+    }
+    save()
 
 }
 
@@ -301,111 +236,20 @@ function load(){
     traditional: true,
     type: 'POST',
     async : false,
-    url: '/public/serledigt',
-    success: function (lerledigt) {
-      erledigt = lerledigt
+    url: '/public/data',
+    success: function (lodeddata) {
+      data = lodeddata
+
+      done = parseInt(data.done)
+      maxHoehe = data.max
+      saved_password = data.password
+      savetime = data.timeleft
+      name1 = data.goal.name
+      height1 = data.goal.height
+      finishtime = data.finishtime
+      
     }
   });
-
-  $.ajax({
-    dataType: "JSON",
-    traditional: true,
-    type: 'POST',
-    async : false,
-    url: '/public/shoehe',
-    success: function (lhoehe) {
-      hoehe = lhoehe
-    }
-  });
-
-  $.ajax({
-    dataType: "JSON",
-    traditional: true,
-    type: 'POST',
-    async : false,
-    url: '/public/serledigt',
-    success: function (lerledigt) {
-      erledigt = lerledigt
-    }
-  });
-
-  $.ajax({
-    dataType: "text",
-    traditional: true,
-    type: 'POST',
-    async : false,
-    url: '/public/saved_passwort',
-    success: function (lsaved_passwort) {
-      saved_passwort = lsaved_passwort
-    }
-  });
-  $.ajax({
-    dataType: "JSON",
-    traditional: true,
-    type: 'POST',
-    url: '/public/smaxHoehe',
-    async : false,
-
-    success: function (lmaxHoehe) {
-      maxHoehe = lmaxHoehe
-    }
-  });
-
-  $.ajax({
-    dataType: "text",
-    traditional: true,
-    type: 'POST',
-    url: '/public/scurrentgift',
-    async : false,
-
-    success: function (lcurrentgift) {
-      currentgift = lcurrentgift
-    }
-  });
-
-
-  $.ajax({
-    dataType: "text",
-    traditional: true,
-    type: 'POST',
-    url: '/public/lsavetime',
-    async : false,
-    success: function (lsavetime) {
-      savetime = lsavetime
-      }
-  });
-
-
-
-
-
-  $.ajax({
-    dataType: "JSON",
-    traditional: true,
-    type: 'POST',
-    url: '/public/sheight1',
-    async : false,
-
-    success: function (lheight1) {
-      height1 = lheight1
-    }
-  });
-
-
-
-  $.ajax({
-    dataType: "text",
-    traditional: true,
-    type: 'POST',
-    async : false,
-
-    url: '/public/scurrentgoal',
-    success: function (lcurrentgoal) {
-      currentgoal = lcurrentgoal
-    }
-  });
-
-
 }
 
 /////////////////////////////
@@ -417,116 +261,25 @@ function load(){
 //                         //                                      
 //                         //                                        
 /////////////////////////////
-function save_erledigt(){
-  $.ajax({
-    dataType: "JSON",
-    traditional: true,
-    type: 'POST',
-    data:{'erledigt': erledigt},
-    cache: false,
-    url: '/public/erledigt',
-
-  });
-  
-}
-
-function save_goalpassed(){
-  $.ajax({
-    dataType: "text",
-    traditional: true,
-    type: 'POST',
-    data:{'goalpassed': goalpassed},
-    cache: false,
-    url: '/public/goalpassed',
-    success: function (goalpassed) {
-      window.location.reload();
-    }
-  });
-}
-
-function save_maxHoehe(){
-  $.ajax({
-    dataType: "JSON",
-    traditional: true,
-    type: 'POST',
-    data:{'maxHoehe': maxHoehe},
-    cache: false,
-    url: '/public/maxHoehe',
-  });
-}
-
-function save_hoehe(){
-  $.ajax({
-    dataType: "JSON",
-    traditional: true,
-    type: 'POST',
-    data:{'hoehe': hoehe},
-    cache: false,
-    url: '/public/hoehe',
-    success: function (hoehe) {
-        window.location.reload();
-    }
-  });
-}
-
-function save_name1(){
-  $.ajax({
-    dataType: "text",
-    traditional: true,
-    type: 'POST',
-    data:{'name1': name1},
-    cache: false,
-    url: '/public/name1',
-    success: function (name1) {
-      window.location.reload();
-    }
-  });
-}
-
-function save_height1(){
+function save() {
+  console.log(data)
+  data.done = done
+  data.max = maxHoehe
+  data.password = saved_password
+  data.timeleft = savetime
+  data.goal.name = name1
+  data.goal.height = height1
+  data.finishtime = finishtime
+  console.log(data)
 
   $.ajax({
     dataType: "JSON",
     traditional: true,
     type: 'POST',
-    data:{'height1': height1},
+    data:{'data': JSON.stringify(data)},
     cache: false,
-    url: '/public/height1',
-  });
-}
+    url: '/public/savedata',
 
-
-function save_currentgoal(){
-  $.ajax({
-    dataType: "text",
-    traditional: true,
-    type: 'POST',
-    data:{'currentgoal': currentgoal},
-    cache: false,
-    url: '/public/currentgoal',
-  });
-}
-
-function save_currentgift(){
-  $.ajax({
-    dataType: "text",
-    traditional: true,
-    type: 'POST',
-    data:{'currentgift': currentgift},
-    cache: false,
-    url: '/public/currentgift',
-  });
-}
-
-function save_savetime(){
-  console.log(savetime)
-  $.ajax({
-    method: "POST",
-    dataType: "text",
-    traditional: true,
-    data:{'savetime': savetime},
-    cache: false,
-    url: '/public/savetime',
   });
 }
 
